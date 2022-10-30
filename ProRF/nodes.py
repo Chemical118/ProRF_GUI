e59e6eee6759b72440b10db32bca566179fbe499
Ryu <wowo0118@sasa.hs.kr>
2022-10-30 23:15:52 +0900

    10/30

--- ProRF/nodes.py
+++ ProRF/nodes.py
@@ -1,8 +1,21 @@
+import numpy
 from ryven.NENV import *
 from julia import ProRF as p
+import numpy as np
 from datetime import datetime as d
 
 widgets = import_widgets(__file__)
+protein_dict = ['vol', 'pI', 'hyd', 'vol, pI', 'vol, hyd', 'pI, hyd', 'all']
+
+
+class ViewData:
+    def __init__(self, val, size, seed):
+        self.val = val
+        self.size = size
+        self.seed = seed
+
+    def get_data(self):
+        return self.val, self.size, self.seed
 
 
 class NodeBase(Node):
@@ -44,7 +57,7 @@ class DualNodeBase(NodeBase):
             'active': self.active
         }
 
-    def set_state(self, data: dict, version):
+    def set_state(self, data: dict):
         self.active = data['active']
 
 
@@ -63,6 +76,7 @@ class ReadData(NodeBase):
         NodeInputBP(label='xlsx_sheet', dtype=dtypes.String(default='Sheet1')),
         NodeInputBP(label='xlsx_title', dtype=dtypes.Boolean(default=True)),
         NodeInputBP(label='seq_loc', dtype=dtypes.String(default='0')),
+        NodeInputBP(label='seq_dict', dtype=dtypes.Choice(default='all', items=protein_dict)),
     ]
     init_outputs = [
         NodeOutputBP('X'),
@@ -99,13 +113,14 @@ class ReadData(NodeBase):
     def update_event(self, inp=-1):
         if (self.input(0) is not None or self.fasta_filepath != '') and \
                 (self.input(1) is not None or self.xlsx_filepath != '') and \
-                self.input(2) != '' and self.input(3) != '' and self.input(4) != '':
+                self.input(2) != '' and self.input(3) != '' and self.input(5) != '' and self.input(6) in protein_dict:
             fasta_loc = self.fasta_filepath if self.input(0) is None else self.input(0)
             xlsx_loc = self.xlsx_filepath if self.input(1) is None else self.input(1)
             xlsx_col = self.input(2)
             xlsx_sheet = self.input(3)
             xlsx_title = self.input(4)
-            seq_loc = self.input(5).strip()
+            seq_loc = self.input(5).replace(' ', '')
+            seq_dict = self.input(6).replace(' ', '').split(',') if ',' in self.input(6) else self.input(6)
             loc = 0
 
             try:
@@ -116,7 +131,8 @@ class ReadData(NodeBase):
             except ValueError:
                 pass
 
-            x, y, lo = p.get_data(p.RF(fasta_loc, xlsx_loc, loc), p.only(xlsx_col), title=xlsx_title, sheet=xlsx_sheet)
+            x, y, lo = p.get_data(p.RF(fasta_loc, xlsx_loc, loc), p.only(xlsx_col), title=xlsx_title, sheet=xlsx_sheet,
+                                  convert=seq_dict)
             self.set_output_val(0, x)
             self.set_output_val(1, y)
             self.set_output_val(2, lo)
@@ -134,6 +150,7 @@ class ReadDataset(NodeBase):
         NodeInputBP(label='xlsx_col', dtype=dtypes.String()),
         NodeInputBP(label='xlsx_sheet', dtype=dtypes.String(default='Sheet1')),
         NodeInputBP(label='xlsx_title', dtype=dtypes.Boolean(default=True)),
+        NodeInputBP(label='seq_dict', dtype=dtypes.Choice(default='all', items=protein_dict)),
     ]
     init_outputs = [
         NodeOutputBP('X'),
@@ -167,16 +184,181 @@ class ReadDataset(NodeBase):
             xlsx_col = self.input(1)
             xlsx_sheet = self.input(2)
             xlsx_title = self.input(3)
+            seq_dict = self.input(4).replace(' ', '').split(',') if ',' in self.input(4) else self.input(4)
 
-            x, y, lo = p.get_data(p.RF(folder_loc), p.only(xlsx_col), title=xlsx_title, sheet=xlsx_sheet)
+            x, y, lo = p.get_data(p.RF(folder_loc), p.only(xlsx_col), title=xlsx_title, sheet=xlsx_sheet,
+                                  convert=seq_dict)
+
             self.set_output_val(0, x)
             self.set_output_val(1, y)
             self.set_output_val(2, lo)
 
 
+class MinMaxNormNode(NodeBase):
+    """Perform Min-Max normalization"""
+
+    title = 'Norm'
+    style = 'small'
+    init_inputs = [
+        NodeInputBP(label='Y'),
+    ]
+    init_outputs = [
+        NodeOutputBP(label='Y'),
+    ]
+
+    def update_event(self, inp=-1):
+        if type(self.input(0)) == np.ndarray:
+            y = self.input(0)
+            self.set_output_val(0, (y - y.min()) / (y.max() - y.min()))
+
+
+def isminmaxable(obj):
+    try:
+        np.min(obj)
+    except:
+        return False
+    return True
+
+
+class RevMinMaxNormNode(NodeBase):
+    """Perform reverse of Min-Max normalization"""
+
+    title = 'Rev norm'
+    style = 'small'
+    init_inputs = [
+        NodeInputBP(label='ref'),
+        NodeInputBP(label='Y'),
+    ]
+    init_outputs = [
+        NodeOutputBP(label='Y'),
+    ]
+
+    def update_event(self, inp=-1):
+        if isminmaxable(self.input(0)) and type(self.input(1)) == np.ndarray:
+            mi, ma = np.min(self.input(0)), np.max(self.input(0))
+            self.set_output_val(0, self.input(1) * (ma - mi) + mi)
+
+
+class FitModelNode(NodeBase):
+    """Fit random forest model"""
+
+    title = 'Fit model'
+    init_inputs = [
+        NodeInputBP(label='X'),
+        NodeInputBP(label='Y'),
+        NodeInputBP(label='feat', dtype=dtypes.Integer(default=6, bounds=(1, 100))),
+        NodeInputBP(label='tree', dtype=dtypes.Integer(default=100, bounds=(1, 2000))),
+        NodeInputBP(label='test_size', dtype=dtypes.Float(default=0.3)),
+        NodeInputBP(label='depth', dtype=dtypes.Integer(default=-1, bounds=(-1, 1000))),
+        NodeInputBP(label='arg', dtype=dtypes.Data(default='', size='m')),
+    ]
+    init_outputs = [
+        NodeOutputBP(label='M'),
+    ]
+
+    def update_event(self, inp=-1):
+        if type(self.input(0)) == np.ndarray and type(self.input(1)) == np.ndarray and \
+                self.input(2) > 0 and self.input(3) > 0 and (self.input(5) == -1 or self.input(5) > 0) and \
+                0 < self.input(4) < 1 and (self.input(6) == '' or type(self.input(6)) == dict):
+
+            arg_dict = self.input(6)
+            if arg_dict == '':
+                arg_dict = dict()
+                seed = p.rand(p.UInt64)
+            else:
+                seed = arg_dict.get('data_state', p.rand(p.UInt64))
+            arg_dict['data_state'] = seed
+            arg_dict['test_size'] = self.input(4)
+            
+            model = p.rf_model(self.input(0), self.input(1), self.input(2), self.input(3), val_mode=True, **arg_dict)
+            self.set_output_val(0, ViewData(model, self.input(4), seed))
+
+
+class PredictNode(NodeBase):
+    """Predict from model and X value"""
+
+    title = 'Predict'
+    style = 'small'
+    init_inputs = [
+        NodeInputBP(label='M'),
+        NodeInputBP(label='X'),
+    ]
+    init_outputs = [
+        NodeOutputBP(label='PY'),
+    ]
+
+    def update_event(self, inp=-1):
+        if type(self.input(0)) == ViewData and type(self.input(1)) == np.ndarray:
+            model, size, seed = self.input(0).get_data()
+            self.set_output_val(0, ViewData(p.parallel_predict(model, self.input(1)), size, seed))
+
+
+class PredictViewNode(NodeBase):
+    """View predict result"""
+
+    title = 'View predict result'
+    init_inputs = [
+        NodeInputBP(label='PY'),
+        NodeInputBP(label='Y'),
+        NodeInputBP(label='nbin', dtype=dtypes.Integer(default=150, bounds=(1, 1000))),
+        NodeInputBP(label='test_set', dtype=dtypes.Boolean(default=True)),
+    ]
+    init_outputs = [
+        NodeOutputBP(label='NRMSE'),
+    ]
+    main_widget_class = widgets.PredictViewWidget
+    main_widget_pos = 'below ports'
+
+    def update_event(self, inp=-1):
+        if self.session.gui and type(self.input(0)) == ViewData and type(self.input(1)) == numpy.ndarray:
+            py, size, seed = self.input(0).get_data()
+            y = self.input(1)
+            n = np.size(y)
+            ed_idx = p.gui_split_index(n, seed, size, self.input(3))
+            py, y = py[ed_idx], y[ed_idx]
+
+            if n > 150:
+                c = p.gui_color_index(py, y, self.input(2))
+                so_idx = np.argsort(c)
+                nrmse_val = self.main_widget().show_result(py[so_idx], y[so_idx], z=c[so_idx])
+            else:
+                nrmse_val = self.main_widget().show_result(py, y)
+            self.set_output_val(0, nrmse_val)
+
+
+class ViewImportanceNode(NodeBase):
+    """View feature importance from model by shapley value"""
+    title = 'View importance'
+    init_inputs = [
+        NodeInputBP(label='M'),
+        NodeInputBP(label='X'),
+        NodeInputBP(label='L'),
+        NodeInputBP(label='imp_iter', dtype=dtypes.Integer(default=60, bounds=(1, 100))),
+        NodeInputBP(label='imp_arg', dtype=dtypes.Data(default='', size='m')),
+        NodeInputBP(label='show_number', dtype=dtypes.Integer(default=15, bounds=(1, 40))),
+    ]
+    init_outputs = [
+        NodeOutputBP(label='F'),
+    ]
+    main_widget_class = widgets.ImportanceViewWidget
+    main_widget_pos = 'below ports'
+
+    def update_event(self, inp=-1):
+        if self.session.gui and type(self.input(0)) == ViewData and type(self.input(1)) == numpy.ndarray and \
+                type(self.input(2)) == list and (self.input(4) == '' or type(self.input(4)) == dict):
+
+            imp_arg_dict = dict() if self.input(4) == '' else self.input(4)
+            imp_arg_dict['imp_iter'] = self.input(3)
+            imp_arg_dict['val_mode'] = True
+
+            model = self.input(0).get_data()[0]
+            f = p.rf_importance(p.RF(''), model, self.input(1), self.input(2), **imp_arg_dict)
+            self.main_widget().show_result(f, self.input(2), self.input(5))
+            self.set_output_val(0, f)
+
+
 class PrintNode(DualNodeBase):
     title = 'Print'
-    version = 'v0.1'
     init_inputs = [
         NodeInputBP(type_='exec'),
         NodeInputBP(dtype=dtypes.Data(size='m')),
@@ -199,5 +381,11 @@ class PrintNode(DualNodeBase):
 export_nodes(
     ReadData,
     ReadDataset,
+    RevMinMaxNormNode,
+    PredictViewNode,
+    ViewImportanceNode,
+    MinMaxNormNode,
+    FitModelNode,
+    PredictNode,
     PrintNode,
 )
--- ProRF/widgets.py
+++ ProRF/widgets.py
@@ -1,13 +1,23 @@
 from ryven.NWENV import *
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib import rcParams
 from qtpy.QtWidgets import QLabel, QPushButton, QFileDialog, QVBoxLayout, QWidget, QTextEdit
 from qtpy.QtGui import QImage, QPixmap, QFont
 from qtpy.QtCore import Signal, QSize, QTimer
 
 import os
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('Qt5Agg')
+
+
+def nrmse(pre, tru):
+    return np.sqrt(np.mean((pre - tru) ** 2)) / (np.max(tru) - np.min(tru))
 
 
 class XLSXInputWidget(IWB, QPushButton):
-
     path_chosen = Signal(str)
 
     def __init__(self, params):
@@ -22,7 +32,7 @@ class XLSXInputWidget(IWB, QPushButton):
             file_path = os.path.relpath(file_path)
         except ValueError:
             return
-        
+
         self.path_chosen.emit(file_path)
 
 
@@ -64,8 +74,83 @@ class FolderInputWidget(IWB, QPushButton):
         self.path_chosen.emit(file_path)
 
 
+class PredictViewWidget(MWB, QWidget):
+    def __init__(self, params):
+        MWB.__init__(self, params)
+        QWidget.__init__(self)
+
+        self.figure = plt.Figure(figsize=(40, 35))
+        self.canvas = FigureCanvasQTAgg(self.figure)
+
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.canvas)
+
+        self.setFixedHeight(350)
+        self.setFixedWidth(400)
+
+    def show_result(self, py, y, z=None):
+        self.figure.clear()
+        rcParams.update({'figure.autolayout': True})
+        ax = self.figure.add_subplot(111)
+        nrmse_val = nrmse(py, y)
+
+        if z is None:
+            ax.scatter(y, py, color="#440154", s=20)
+        else:
+            im = ax.scatter(y, py, c=z, s=3)
+            plt.colorbar(im, ax=ax)
+
+        # ax.set_title("Random Forest Regression Result")
+        ax.set_xlabel("True Values")
+        ax.set_ylabel("Predictions")
+        ax.axis("equal")
+        ax.axis("square")
+        ax.set_xlim(-max(0, -ax.get_xlim()[0]), ax.get_xlim()[1])
+        ax.set_ylim(-max(0, -ax.get_ylim()[0]), ax.get_ylim()[1])
+        ax.annotate("NRMSE : %.4f" % nrmse_val, ((np.asarray([0.97, 0.03]) @ np.asarray(ax.get_xlim())),
+                                                 (np.asarray([0.05, 0.95]) @ np.asarray(ax.get_ylim()))))
+        ax.plot([-1000, 1000], [-1000, 1000], color="black")
+        self.canvas.draw()
+        return nrmse_val
+
+
+class ImportanceViewWidget(MWB, QWidget):
+    def __init__(self, params):
+        MWB.__init__(self, params)
+        QWidget.__init__(self)
+
+        self.figure = plt.Figure(figsize=(40, 35))
+        self.canvas = FigureCanvasQTAgg(self.figure)
+
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.canvas)
+
+        self.setFixedHeight(350)
+        self.setFixedWidth(400)
+
+    def show_result(self, f, lo, show_number):
+        self.figure.clear()
+        rcParams.update({'figure.autolayout': True})
+        ax = self.figure.add_subplot(111)
+        lo = np.asarray(lo)
+
+        data_len = np.size(f)
+        f /= np.max(f)
+        show_number = min(data_len, show_number)
+        sorted_idx = np.argsort(f)[::-1]
+        bar_pos = np.asarray(range(data_len, 0, -1)) - 0.5
+        ax.barh(bar_pos[0:show_number], f[sorted_idx][0:show_number], align="center")
+        ax.set_yticks(bar_pos[0:show_number], lo[sorted_idx][0:show_number])
+        ax.set_xlabel("Feature Importance")
+        ax.set_ylabel("Amino acid Location")
+
+        self.canvas.draw()
+
+
 export_widgets(
     XLSXInputWidget,
     FASTAInputWidget,
     FolderInputWidget,
+    PredictViewWidget,
+    ImportanceViewWidget,
 )
